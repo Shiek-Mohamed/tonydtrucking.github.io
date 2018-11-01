383588f0f53332eedbb5a4a7dab7c4986dda218c
Shiek-Mohamed <42737417+Shiek-Mohamed@users.noreply.github.com>
2018-11-01 15:35:38 -0400

    First Website Sample

---
+++ demo.js
@@ -0,0 +1,19 @@
+$( "h2" ).clone().prependTo( "footer" );
+
+$( ".jumbotron" ).clone().prependTo( "footer" );
+
+$(".logo").hide();
+
+$("h2").css({border: "3px solid red"});
+
+$(".box").css({border: "10px solid red"});
+
+//$("#jumbotron").css({border: "5px solid yellow"});
+
+//$("footer").addClass("big");
+
+$("footer").addClass("big bright heavy red");
+
+$("footer").removeClass("big");
+
+$(".box").html("<h1>Hello World of Trucking</h1>");
